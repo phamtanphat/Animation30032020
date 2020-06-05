3508bfcbf5052cd5b0d83e270094edca1bcea799
phatdroid94 <phatdroid94@gmail.com>
2020-06-05 21:32:45 +0700

    finish

--- app/src/main/java/com/example/animation30032020/MainActivity.java
+++ app/src/main/java/com/example/animation30032020/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.animation30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +14,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView mImgalpha,mImgScale,mImgTransition;
+    ImageView mImgalpha,mImgScale,mImgTransition,mImgRotate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         mImgalpha = findViewById(R.id.imageAlpha);
         mImgScale = findViewById(R.id.imageScale);
         mImgTransition = findViewById(R.id.imageTransition);
+        mImgRotate = findViewById(R.id.imageRotate);
 
         mImgalpha.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +40,17 @@ public class MainActivity extends AppCompatActivity {
         mImgTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startAnimation(R.anim.animation_tranisition,v);
+//                startAnimation(R.anim.animation_tranisition,v);
+//                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+//                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+//                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+            }
+        });
+
+        mImgRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation(R.anim.animation_rotate,v);
             }
         });
     }
